204d02cca0cd9a076c2ab7aaef9ec1584fce29f6
Vladyslav Konotovskyi <konotovsky@gmail.com>
2026-02-16 12:30:59 +0000

    Add Features interaction and Email validation

--- src/main.js
+++ src/main.js
@@ -16,6 +16,58 @@ closeBtn.addEventListener("click", () => {
   overlay.classList.add("hidden");
 });
 
+//Features
+const features = [
+  {
+    img: "/images/illustration-features-tab-1.svg",
+    title: "Bookmark in one click",
+    description:
+      "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
+  },
+  {
+    img: "/images/illustration-features-tab-2.svg",
+    title: "Intelligent search",
+    description:
+      "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
+  },
+  {
+    img: "/images/illustration-features-tab-3.svg",
+    title: "Share your bookmarks",
+    description:
+      "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
+  },
+];
+
+const links = document.querySelectorAll(".feature-link");
+const image = document.getElementById("feature-img");
+const title = document.getElementById("feature-title");
+const description = document.getElementById("feature-description");
+
+function setActiveFeature(index) {
+  const feature = features[index];
+
+  image.src = feature.img;
+  title.textContent = feature.title;
+  description.textContent = feature.description;
+
+  links.forEach((l) => {
+    l.classList.remove("border-red-400", "text-red-400");
+    l.classList.add("border-transparent");
+  });
+
+  links[index].classList.remove("border-transparent");
+  links[index].classList.add("border-red-400", "text-red-400");
+}
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    setActiveFeature(link.dataset.index);
+  });
+});
+
+setActiveFeature(0);
+
 //Accordion
 const accordion = document.querySelector(".accordion");
 const buttons = accordion.querySelectorAll("button");
@@ -48,3 +100,46 @@ buttons.forEach((button) => {
     }
   });
 });
+
+//Email error
+const form = document.querySelector("form");
+const emailInput = document.getElementById("email");
+const error = document.getElementById("error");
+const submit = document.getElementById("sendForm");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const email = emailInput.value.trim();
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailPattern.test(email)) {
+    showError();
+  } else {
+    hideError();
+
+    form.submit();
+  }
+});
+
+emailInput.addEventListener("input", () => {
+  if (emailInput.checkValidity()) {
+    hideError();
+  }
+});
+
+function showError() {
+  error.classList.remove("hidden");
+  emailInput.classList.remove("border-transparent");
+  emailInput.classList.add("border-red-400");
+  emailInput.classList.remove("rounded-b-[5px]");
+  submit.classList.add("mt-4", "sm:mt-0");
+}
+
+function hideError() {
+  error.classList.add("hidden");
+  emailInput.classList.remove("border-red-400");
+  emailInput.classList.add("rounded-b-[5px]");
+  emailInput.classList.add("border-transparent");
+  submit.classList.remove("mt-4", "sm:mt-0");
+}
